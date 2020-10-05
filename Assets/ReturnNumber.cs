cceeb78fd7ed2a97390b206a3f86c033f526b468
Nathaniel <nathbrandal@gmail.com>
2020-10-06 00:19:46 +0200

    fully implemented numpad

---
+++ Assets/ReturnNumber.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ReturnNumber : MonoBehaviour
+{
+    public void NumberFromTextElement()
+	{
+		var text = GetComponentInChildren<TextMeshProUGUI>();
+		if (text.text.IsNumeric())
+		{
+			this.transform.parent.parent.GetComponent<NumpadInput>().AddNumberToList(int.Parse(text.text));
+		}
+	}
+}
