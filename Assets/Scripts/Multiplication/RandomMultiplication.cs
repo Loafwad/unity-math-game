f4fb2e2afee066d43508a78885bfea7c299baaea
Nathaniel <nathbrandal@gmail.com>
2020-10-08 01:34:05 +0200

    added functionality to submit answer and generate a new math question depending on wether it's right or wrong

---
+++ Assets/Scripts/Multiplication/RandomMultiplication.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RandomMultiplication : MonoBehaviour
+{
+	[SerializeField]
+	private int min, max;
+
+	[SerializeField]
+	private TextMeshProUGUI text;
+	[SerializeField]
+	private NumpadInput numpadInput;
+
+	private int firstNumber;
+	private int secondNumber;
+	private void Start()
+	{
+		GenerateNewMathProblem();
+	}
+
+	public void GenerateNewMathProblem()
+	{
+		firstNumber = Random.Range(min, max);
+		secondNumber = Random.Range(min, max);
+		ParseToText();
+		Debug.Log("Answer is " + Answer());
+	}
+
+	private int Answer()
+	{
+		return firstNumber * secondNumber;
+	}
+
+	public void SubmitAnswer()
+	{
+		int numbers = numpadInput.AllNumbers();
+		int amountOfNumbersInList = numpadInput.listOfNumbers.Count;
+		if (AnswerIsSame(numbers))
+		{
+			Debug.Log("correct");
+			for (int i = 0; i < amountOfNumbersInList; i++)
+			{
+				numpadInput.RemoveLastNumber();
+			}
+			GenerateNewMathProblem();
+		}
+		else
+		{
+			Debug.Log("wrong");
+			for (int i = 0; i < amountOfNumbersInList; i++)
+			{
+				numpadInput.RemoveLastNumber();
+			}
+		}
+	}
+	private bool AnswerIsSame(int answer)
+	{
+		if(answer == Answer())
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	private void ParseToText()
+	{
+		text.text = firstNumber.ToString() + " x " + secondNumber.ToString() + " =";
+	}
+}
