7c59fea5884a9c92dfc399e9b6f2ca34f596aa06
Nathaniel <nathbrandal@gmail.com>
2020-10-15 00:19:14 +0200

    started implementation of options menu

---
+++ Assets/ToggleCheckmark.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCheckmark : MonoBehaviour
+{
+	private Transform checkmark;
+	private bool toggle;
+
+	private void Start()
+	{
+		checkmark = gameObject.transform.GetChild(2);
+	}
+	public void Toggle()
+	{
+		if (toggle)
+		{
+			checkmark.gameObject.SetActive(true);
+			toggle = !toggle;
+		}
+		else
+		{
+			checkmark.gameObject.SetActive(false);
+		}
+	}
+}
