394bf0f26c4dba7b5a2028265f000fe80593c2a3
Nathaniel <nathbrandal@gmail.com>
2020-10-15 02:40:47 +0200

    added saving to options menu

--- Assets/ToggleCheckmark.cs
+++ Assets/ToggleCheckmark.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class ToggleCheckmark : MonoBehaviour
 {
+	[SerializeField]
+	private string currentOption;
 	private Transform checkmark;
 	private bool toggle;
 
 	private void Start()
 	{
 		checkmark = gameObject.transform.GetChild(1);
+		LoadSettings();
 	}
 	public void Toggle()
 	{
 		toggle = !toggle;
+		SaveSettings();
 		if (toggle)
 		{
 			checkmark.gameObject.SetActive(true);
@@ -22,5 +26,39 @@ public class ToggleCheckmark : MonoBehaviour
 		{
 			checkmark.gameObject.SetActive(false);
 		}
+	}
+	
+	public void SaveSettings()
+	{
+		PlayerPrefsX.SetBool(currentOption, toggle);
+	}
+
+	public void LoadSettings()
+	{
+		PlayerPrefsX.GetBool(currentOption);
+	}
+}
+
+//move to seperate script
+public class PlayerPrefsX
+{
+	public static void SetBool(string name, bool booleanValue)
+	{
+		PlayerPrefs.SetInt(name, booleanValue ? 1 : 0);
+	}
+
+	public static bool GetBool(string name)
+	{
+		return PlayerPrefs.GetInt(name) == 1 ? true : false;
+	}
+
+	public static bool GetBool(string name, bool defaultValue)
+	{
+		if (PlayerPrefs.HasKey(name))
+		{
+			return GetBool(name);
+		}
+
+		return defaultValue;
 	}
 }
