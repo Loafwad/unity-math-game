b93e6492192f7f773763fe5adabe899f408fc4ef
Nathaniel <nathbrandal@gmail.com>
2020-10-15 02:49:28 +0200

    cleaned up hierachy and files

---
+++ Assets/Scripts/Menu/ToggleCheckmark.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCheckmark : MonoBehaviour
+{
+	[SerializeField]
+	private string currentOption;
+	private Transform checkmark;
+	private bool toggle;
+
+	private void Start()
+	{
+		checkmark = gameObject.transform.GetChild(1);
+		LoadSettings();
+	}
+	public void Toggle()
+	{
+		toggle = !toggle;
+		SaveSettings();
+		if (toggle)
+		{
+			checkmark.gameObject.SetActive(true);
+		}
+		else
+		{
+			checkmark.gameObject.SetActive(false);
+		}
+	}
+	
+	public void SaveSettings()
+	{
+		PlayerPrefsX.SetBool(currentOption, toggle);
+	}
+
+	public void LoadSettings()
+	{
+		PlayerPrefsX.GetBool(currentOption);
+	}
+}
+
---
+++ Assets/Scripts/UserInterface/FeedbackText.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class FeedbackText : MonoBehaviour
+{
+	[SerializeField]
+	private float correctFadeTime = 0.5f;
+	[SerializeField]
+	private TextMeshProUGUI correct;
+
+	[SerializeField]
+	private float wrongFadeTime = 0.5f;
+	[SerializeField]
+	private TextMeshProUGUI wrong;
+
+	bool toggle;
+    public void ToggleCorrect()
+	{
+		toggle = !toggle;
+		correct.gameObject.SetActive(toggle);
+		StartCoroutine(FadeTextToZeroAlpha(correctFadeTime, correct));
+		toggle = !toggle;
+	}
+
+	public void ToggleWrong()
+	{
+		toggle = !toggle;
+		wrong.gameObject.SetActive(toggle);
+		StartCoroutine(FadeTextToZeroAlpha(wrongFadeTime, wrong));
+		toggle = !toggle;
+	}
+
+	public IEnumerator FadeTextToFullAlpha(float t, TextMeshProUGUI i)
+	{
+		i.color = new Color(i.color.r, i.color.g, i.color.b, 0);
+		while (i.color.a < 1.0f)
+		{
+			i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a + (Time.deltaTime / t));
+			yield return null;
+		}
+	}
+
+	public IEnumerator FadeTextToZeroAlpha(float t, TextMeshProUGUI i)
+	{
+		i.color = new Color(i.color.r, i.color.g, i.color.b, 1);
+		while (i.color.a > 0.0f)
+		{
+			i.color = new Color(i.color.r, i.color.g, i.color.b, i.color.a - (Time.deltaTime / t));
+			yield return null;
+		}
+	}
+
+}
