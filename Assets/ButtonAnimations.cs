399fcc54c017e7dd2bd5e9dd2553129525601176
Nathaniel <nathbrandal@gmail.com>
2020-10-20 00:28:53 +0200

    added slide glow feature to button animations

--- Assets/ButtonAnimations.cs
+++ Assets/ButtonAnimations.cs
@@ -4,25 +4,14 @@ using UnityEngine;
 
 public class ButtonAnimations : MonoBehaviour
 {
-	//[Header("Hover Animation")]
-	//public AnimationCurve animCurveHover;
-	//public float animDurationHover = 1;
-	//public float animDelayHover;
-
 	[Header("Click Animation")]
 	public AnimationCurve animCurveClick;
 	public float animDurationClick = 1;
 
-	[Header("Navigation Menus")]
-	public GameObject currentMenu;
-	public GameObject nextMenu;
-	/*
-	public void Hover(float size)
-	{
-		var scale = new Vector3(size, size, gameObject.transform.localScale.z);
-		LeanTween.scale(gameObject, scale, animDurationHover).setDelay(animDelayHover).setLoopOnce().setEase(animCurveHover);
-	}
-	*/
+	[Header("Slider Animation")]
+	public AnimationCurve animCurveSlide;
+	public float animDurationSlide = 1;
+
 
 	public void Click(float size)
 	{
@@ -33,7 +22,12 @@ public class ButtonAnimations : MonoBehaviour
 	private void Toggle()
 	{
 		gameObject.transform.localScale = new Vector3(1, 1, 1);
-		//currentMenu.SetActive(false);
-		//nextMenu.SetActive(true);
+	}
+
+	public void Slide()
+	{
+		this.gameObject.SetActive(true);
+		this.gameObject.transform.position = new Vector3(-500, gameObject.transform.position.y, gameObject.transform.position.z);
+		LeanTween.moveX(this.gameObject, 1000, animDurationSlide);
 	}
 }
